6c1cef64cb5f2f80c5f434b2e8c83cf867b77dd6
harshgehlot-code <er.harsh.gehlot1@gmail.com>
2025-08-15 18:44:45 +0000

    string

---
+++ 01_basics/5_string.js
@@ -0,0 +1,30 @@
+
+// const name = "harsh"
+// console.log(`hello ${name}`);
+
+
+// const gameName = new String("harsh")
+
+// console.log(gameName)
+
+// console.log(gameName[0])
+// console.log(gameName.length)
+
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('r'))
+
+// const newString = gameName.substring(0,3)
+
+// const anotherStiring = gameName.slice(-2,5)
+
+// console.log(newString)
+// console.log(anotherStiring)
+
+const newStrings = "      harsh         "
+
+console.log(newStrings)
+console.log(newStrings.trim())
+
+const url = "https://harshgehlot%20harsh.com"
+
+console.log(url.replace('%20', '-'))
