c7f0c217b14e74c49ae1be596abf88aa775d4f50
harshgehlot-code <er.harsh.gehlot1@gmail.com>
2025-08-15 14:17:19 +0000

    var and dataType

---
+++ 01_basics/1_var.js
@@ -0,0 +1,6 @@
+const accountID = 123
+let accountEmail = "harsh@google.com"
+var acounrtCity = "indore"
+
+console.table([accountID, accountEmail, acounrtCity])
+
---
+++ 01_basics/2_dataType.js
@@ -0,0 +1,22 @@
+
+"use strict" ; // treat all JS code as never version 
+
+// alert("hello") we are using node js , not browser
+
+let name = "hitesh"
+let age = 24
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint for long integer
+// stirng => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => it have nothing in varialbe or no value
+// symbol => for identified component / unique
+
+// object 
+
+console.log(typeof "hitech")
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
