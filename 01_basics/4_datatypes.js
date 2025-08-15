65ed69598aae2de31a0331d53d161ea710d3daab
harshgehlot-code <er.harsh.gehlot1@gmail.com>
2025-08-15 18:07:55 +0000

    data_types

---
+++ 01_basics/4_datatypes.js
@@ -0,0 +1,26 @@
+
+// Primitive 
+
+//  Non Primitive
+
+// Stack(primitive) and Heap(Non Primitive)
+
+let myYtname = "harsh"
+
+let anothername = myYtname
+anothername = "harsh1"
+
+console.log(myYtname)
+console.log(anothername)
+
+
+let userOne = {
+    "email" : "harah@g.com",
+    "skill" : "python"
+}
+
+let userTwo = userOne
+userTwo.email = "harsh1@g.com"
+
+console.log(userOne.email)
+console.log(userTwo.email);
