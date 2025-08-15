512ba002898d454a17bd58490f73d8ef0e5be629
harshgehlot-code <er.harsh.gehlot1@gmail.com>
2025-08-15 15:14:43 +0000

    C_O

---
+++ 01_basics/3_conversionOperation.js
@@ -0,0 +1,16 @@
+// let score  = "33aa"
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber))
+
+// console.log(valueInNumber)
+
+// "33" => 33
+//  "33aba" => NaN , not a number
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
