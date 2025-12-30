730f47c80453e9f5375c05753579d03b7ce0cb70
Harsh Gehlot <er.harsh.gehlot1@gmail.com>
2025-12-30 19:30:25 +0000

    updates and learnings

--- 01_basics/6_nums_.js
+++ 01_basics/6_nums_.js
@@ -1,25 +1,24 @@
 const score  = 400 
 
-console.log(score);
+// console.log(score);
 
-const balance = new Number(score)
-console.log(balance);
+// const balance = new Number(score)
+// console.log(balance);
 
-console.log(balance.toString().length);
+// console.log(balance.toString().length);
 
-console.log(balance.toFixed(2));
+// console.log(balance.toFixed(2));
 
 // const num = 31.98844
-console.log(num.toPrecision(4));
+// console.log(num.toPrecision(4));
 
 // const nums = 100000000
 // console.log(nums.toLocaleString('en-IN'));
 
-// =========================
-
+// ========================= Msth library
 
 // console.log(Math.abs(-4));
-// console.log(Math.round(4.3));
+// console.log(Math.round(4.6));
 // console.log(Math.ceil(5.9));
 // console.log(Math.floor(5.9));
 
--- 01_basics/7_dates.js
+++ 01_basics/7_dates.js
@@ -1,10 +1,11 @@
 
 
 let mydate = new Date() // Create a new date object
-
+// console.log(mydate)
 // console.log(mydate.toString());
 // console.log(mydate.toDateString());
 // console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
 
 
 // let mydate2 = new Date(2024, 0, 1) // January is month 0
@@ -12,20 +13,20 @@ let mydate2 = new Date('2024-01-14') // ISO format
 // console.log(mydate2.toLocaleString());
 
 let Timestamp = Date.now() // Get the timestamp in milliseconds
-// console.log(mydate2.getTime());
 // console.log(Timestamp);
+// console.log(mydate2.getTime());
 
 // console.log(mydate2.getFullYear()); // Get the year
 
 // console.log(Math.floor(Date.now()/1000));
 
-newDate.toLocaleString('default',{
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    TimeZone: 'UTC'
-})
+// newDate.toLocaleString('default',{
+//     weekday: 'long',
+//     year: 'numeric',
+//     month: 'long',
+//     day: 'numeric',
+//     hour: '2-digit',
+//     minute: '2-digit',
+//     second: '2-digit',
+//     TimeZone: 'UTC'
+// })
